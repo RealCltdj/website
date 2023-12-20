fe9e6c2deefcee2b0d51ac798a4053d13ba884ea
Tyler Jones <tyler.jones@uvu.edu>
2023-12-20 06:49:07 -0700

    basic page complete

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import './assets/main.css'
+// import './assets/main.css'
 
 // Import SCSS styles
 import './scss/styles.scss'
@@ -6,14 +6,39 @@ import './scss/styles.scss'
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
 
+import "bootstrap-icons/font/bootstrap-icons.css";
+
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
 
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import all icons
+import * as fa_free_solid from '@fortawesome/free-solid-svg-icons'
+import * as fa_free_regular from '@fortawesome/free-regular-svg-icons'
+import * as fa_free_brands from '@fortawesome/free-brands-svg-icons'
+
+[...Object.entries(fa_free_solid), ...Object.entries(fa_free_regular), ...Object.entries(fa_free_brands)].forEach(([name, module]) => {try {library.add(module)} catch {console.log(`failed to load icon: ${name}`)}})
+*/
+
+// import specific icons
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import {  } from '@fortawesome/free-regular-svg-icons'
+import { faYoutube } from '@fortawesome/free-brands-svg-icons'
+
+/* add icons to library */
+library.add(faUserSecret, faYoutube)
+
 const app = createApp(App)
 
+app.component('font-awesome-icon', FontAwesomeIcon)
 app.use(createPinia())
 app.use(router)
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,18 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <div class="container mt-5">
+      <div class="row justify-content-center">
+        <div class="col-md-6 text-center">
+          <h1 class="display-4">Welcome to My Webpage</h1>
+          <h3><i class="bi bi-link"></i> Links <i class="bi bi-link"></i></h3>
+          <div class=""></div>
+          <a type="button" class="btn btn-danger" href="http://www.youtube.com/@cltdj"><font-awesome-icon :icon="['fa-brands', 'youtube']" /> YouTube</a>
+        </div>
+      </div>
+    </div>
   </main>
 </template>
