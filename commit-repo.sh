f46830d560428ab961598a4f7f529f087fafbb72
RealCltdj <51815553+RealCltdj@users.noreply.github.com>
2023-12-20 07:36:30 -0700

    Update and rename commit-disp.sh to commit-repo.sh

---
+++ commit-repo.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd dist
+git config --global user.email "cltdjgaming+CI@gmail.com"
+git config --global user.name "RealCltdj"
+git init
+git add -A
+git commit -m 'rebuilt vue app'
+git push -f https://RealCltdj:$1@github.com/RealCltdj/website.git master:prod
